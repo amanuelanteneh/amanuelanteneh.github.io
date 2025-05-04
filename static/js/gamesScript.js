b2e4cba353564ed8de4ddbb4db44ca57a9dd7900
Amanuel Anteneh <asa2rc@virginia.edu>
2025-05-04 09:17:31 -0400

    more goated games that i remembered

--- static/js/gamesScript.js
+++ static/js/gamesScript.js
@@ -1,5 +1,5 @@
 
-var albumPics = ["https://assets-prd.ignimgs.com/2023/01/23/mass-effect-legendary-edition-blogroll-1608133731075-1674478486620.jpg",
+var gamePics = ["https://assets-prd.ignimgs.com/2023/01/23/mass-effect-legendary-edition-blogroll-1608133731075-1674478486620.jpg",
                 "https://assets1.ignimgs.com/thumbs/userUploaded/2017/3/14/bioticcombos-1489534140465.jpg",
                 "https://assets-prd.ignimgs.com/2022/02/23/eldenring-review-blogroll-1645576973659.jpg",
                 "https://assets1.ignimgs.com/thumbs/userUploaded/2020/7/14/halo3-multiplayerpcblogroll-1594760905664.jpg", 
@@ -8,17 +8,21 @@ var albumPics = ["https://assets-prd.ignimgs.com/2023/01/23/mass-effect-legendar
                 "https://assets1.ignimgs.com/thumbs/userUploaded/2018/5/17/maxresdefault-1526594886194.jpg",
                 "https://sm.ign.com/t/ign_ap/articlepage/p/ps4-xbox-one-get-6-player-dark-souls-ii-scholar-of/ps4-xbox-one-get-6-player-dark-souls-ii-scholar-of_kgn4.1280.jpg",
                 "https://assets-prd.ignimgs.com/2021/07/08/1280x720-6e3-1441008848228jpg-9b3dfc-1625749927174.jpg",
+                "https://assets-prd.ignimgs.com/2023/02/08/infinityblade1-1675816276323.jpg",
+                "https://assets-prd.ignimgs.com/2023/02/08/infinityblade2-1675816241246.jpg",
+                "https://assets-prd.ignimgs.com/2023/02/08/infinity2-1675816251418.jpg",
                 'https://oyster.ignimgs.com/wordpress/stg.ign.com/2013/04/injustice_040913_1600.jpg',
                 'https://assets-prd.ignimgs.com/2023/06/10/injustice2-1024-576-1489108566238-1686441365530.jpg',
-                'https://assets1.ignimgs.com/thumbs/userUploaded/2022/2/1/destinythumb-1607620044156-1643715089550.jpeg'
+                'https://assets1.ignimgs.com/thumbs/userUploaded/2022/2/1/destinythumb-1607620044156-1643715089550.jpeg',
+
    
 ];
-var albumPicElements = document.getElementsByClassName("album-pic");
+var gamePicElements = document.getElementsByClassName("album-pic");
 
 
 $(document).ready(function() {
-    for (let i=0; i < albumPics.length; i++) {
-        albumPicElements[i].style.backgroundImage = 'url("' + albumPics[i] + '")';       
+    for (let i=0; i < gamePics.length; i++) {
+        gamePicElements[i].style.backgroundImage = 'url("' + gamePics[i] + '")';       
     }
 
   });
